4adaa78b7ac35d16422e0581eb5fa26432bda771
vibecodingchile <contacto@vibecodingchile.com>
2026-01-02 05:47:38 -0300

    Create script.js

---
+++ script.js
@@ -0,0 +1,5 @@
+document.getElementById('contact-form').addEventListener('submit', function(e) {
+  e.preventDefault();
+  alert('Gracias por tu mensaje 💖 Nos pondremos en contacto pronto.');
+  this.reset();
+});
